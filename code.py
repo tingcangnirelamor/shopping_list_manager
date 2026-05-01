5c149da84ae51f786de4882a2536dbee0fef484d
tingcangnirelamor <tingcang.nirelamor@gmail.com>
2026-05-01 13:57:25 +0000

    student B

--- code.py
+++ code.py
@@ -7,3 +7,25 @@ except FileExistsError:
 
     with open("shopping_list.txt", "w") as f:
         f.write("Overwritten existing file.")
+
+        while True:
+            
+            try:
+                print("Menu")
+                print("1. Add an item.")
+                print("2. Change an item.")
+                print("3. Remove an item.")    
+                print("4. Exit.")
+
+                choice = int(input("Enter a choice: "))
+            
+            except ValueError:
+                    print("Enter a valid choice.")
+            break
+    
+    while True:
+        if choice == 1:
+            itemlist = input("Enter an item: ")
+            with open("shopping_list.txt", "a") as f:
+                f.write(itemlist + "\n")
+                break
